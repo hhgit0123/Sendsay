b11ca6d0bb1343bd59460f1c78646eced8cf5a56
tempmail12345git <67006526+tempmail12345git@users.noreply.github.com>
2020-06-28 16:53:12 +0300

    deleted files

--- src/MainPage/console/DraggableSplitter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import VerticalDotes from "assets/VerticalDotes";
-export default function DraggableSplitter(props) {
-  return (
-    <div onMouseDown={props.drag} className="console-draggableSplitter">
-      <VerticalDotes />
-    </div>
-  );
-}
--- src/reducers/root.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authReducer.js";
-import historyReducer from "./historyReducer.js";
-import consoleReducer from "./consoleReducer.js";
-export default combineReducers({
-  auth: authReducer,
-  history: historyReducer,
-  console: consoleReducer,
-});
